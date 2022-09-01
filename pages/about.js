a1a1d50202d14ef08ff57e474c1fd66605d153a7
eyo25 <qutuzm@naver.com>
2022-09-02 02:38:04 +0900

    #0.1 next-intro

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import { useRouter } from "next/router";
+import Seo from "../components/Seo";
+
+export default function Potato() {
+  return (
+    <>
+      <Seo title="about"></Seo>
+      <div>about us</div>
+    </>
+  );
+}
